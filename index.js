c27c69dc71e57111782ce70934b4fa0779793aa7
starquill <ajay.2201075cs@iiitbh.ac.in>
2025-10-23 19:37:37 +0530

    Initialise Express server and package.json

---
+++ index.js
@@ -0,0 +1,12 @@
+const express=require('express')
+const app=express()
+const port=3000;
+
+app.use(express.json())
+app.get('/',(req,res)=>{
+    res.send('Hello from the inventory API!')
+})
+
+app.listen(port,()=>{
+    console.log(`Server running on http://localhost:${port}`)
+})
