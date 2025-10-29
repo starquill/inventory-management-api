5673c5adddae6127151cc0372b66a9cce1a2e544
starquill <ajay.2201075cs@iiitbh.ac.in>
2025-10-29 21:46:39 +0530

    Add GET(one), PUT, and DELETE routes for items

--- controllers/itemControllers.js
+++ controllers/itemControllers.js
@@ -30,7 +30,61 @@ const getAllItems=async(req,res)=>{
     }
 }
 
+const getItemById=async (req,res)=>{
+    try{
+        const { id }=req.params;
+        const item=await Item.findById(id).populate('category');
+        if(!item)
+            res.status(404).json({message:'Item not found'})
+        res.status(200).json(item);
+    }catch(error){
+        res.status(500).json({message:"Server Error",error:error.message})
+    }
+}
+
+const updateItem=async (req,res)=>{
+    try{
+        const { id }=req.params;
+        const {name,quantity,category:categoryID}=req.body;
+        const updatedFields={};
+        if(name) updatedFields.name=name;
+        if(quantity) updatedFields.quantity=quantity;
+        if(categoryID){
+            const categoryExists=await Category.findById(categoryID);
+            if(!categoryExists)
+                return res.status(404).json({message:'New category not found'});
+            updatedFields.category=categoryID;
+        }
+        const updatedItem=await Item.findByIdAndUpdate(
+            id,
+            updatedFields,
+            {new:true}
+        )
+        if(!updateItem)
+            return res.status(404).json({ message: 'Item not found' });
+        res.status(200).json(updatedItem);
+    }catch(error){
+        res.status(500).json({message:"Server Error",error:error.message})
+    }
+}
+
+const deleteItem=async (req, res) => {
+    try {
+      const { id }=req.params;
+      const deletedItem=await Item.findByIdAndDelete(id);
+      if (!deletedItem){
+        return res.status(404).json({ message: 'Item not found' });
+      }
+      res.status(200).json({ message: 'Item deleted successfully' });
+    } catch (error) {
+      res.status(500).json({ message: 'Server Error', error: error.message });
+    }
+  };
+
 module.exports={
     createItem,
-    getAllItems
+    getAllItems,
+    getItemById,
+    updateItem,
+    deleteItem
 }
--- routes/item.js
+++ routes/item.js
@@ -1,9 +1,18 @@
 const express=require('express')
 const router=express.Router()
 
-const { createItem,getAllItems } = require('../controllers/itemControllers')
+const { 
+    createItem, 
+    getAllItems,
+    getItemById,
+    updateItem,
+    deleteItem
+ } = require('../controllers/itemControllers')
 
 router.post('/',createItem);
 router.get('/',getAllItems);
+router.put('/:id',updateItem);
+router.delete('/:id',deleteItem);
+router.get('/:id',getItemById);
 
 module.exports=router;
